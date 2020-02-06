5e59d5caa3f80fe634a7c9a4e442362e1bf5359c
mrsimpson <github@beimir.net>
2020-02-06 14:52:37 +0100

    Support multiple reminders

--- src/manipulateEvents.js
+++ src/manipulateEvents.js
@@ -5,14 +5,19 @@ function manipulateEvents(events, operations) {
     events.forEach(event => {
         if (operations) {
             if (operations.remindBeforeStart) {
-                const alarm = new ical.Component('valarm')
-                alarm.addPropertyWithValue('action', 'DISPLAY')
-                const trigger = alarm.addPropertyWithValue('trigger', `-PT${operations.remindBeforeStart}M`)
-                if(operations.reminderDescription){
-                    alarm.addPropertyWithValue('description', operations.reminderDescription)
-                }
-                trigger.setParameter('related', 'START')
-                event.addSubcomponent(alarm)
+                const reminders = Array.isArray(operations.remindBeforeStart)
+                    ? operations.remindBeforeStart
+                    : new Array(operations.remindBeforeStart.toString())
+                reminders.forEach(r => {
+                    const alarm = new ical.Component('valarm')
+                    alarm.addPropertyWithValue('action', 'DISPLAY')
+                    const trigger = alarm.addPropertyWithValue('trigger', `-PT${r}M`)
+                    if (operations.reminderDescription) {
+                        alarm.addPropertyWithValue('description', operations.reminderDescription)
+                    }
+                    trigger.setParameter('related', 'START')
+                    event.addSubcomponent(alarm)
+                })
             }
         }
         manipulatedEvents.push(event)
